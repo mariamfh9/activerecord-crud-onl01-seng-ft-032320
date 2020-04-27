d7f52b1e086d016e3d6cbb3602f45a0a75bbfbb4
Mariam Haider <mfh.haider@gmail.com>
2020-04-27 16:41:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,7 +42,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  movie = Movie.size
+  movie = Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
@@ -62,7 +62,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  
 end
 
 def can_be_found_updated_and_saved
