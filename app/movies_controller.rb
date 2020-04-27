cd4fe05c01e3d471d9181e95bef7bf79551ac807
Mariam Haider <mfh.haider@gmail.com>
2020-04-27 16:39:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,15 +34,15 @@ def can_be_created_in_a_block(args = __)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie = Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  movie = Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
